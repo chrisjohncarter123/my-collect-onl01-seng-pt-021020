41f94c322af2b705458dcf05e1e503b55fa98608
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-16 09:47:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+  i = 0
+  
+  while i < array.length
+    array << yield(array[i])
+    i += 1
+  end
+  
+end
 
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+puts array
+
+#["Tim", "Tom", "Jim"]
